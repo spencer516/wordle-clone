3a592390195cc0ef6b114db08d27c4eee7c454ec
Spencer Price <spencer516@gmail.com>
2022-01-16 10:04:53 -0500

    remove purgeCSS
    Signed-off-by: Spencer Price <spencer516@gmail.com>

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,19 +1,6 @@
 'use strict';
 
 const EmberApp = require('ember-cli/lib/broccoli/ember-app');
-const isProduction = EmberApp.env() === 'production';
-
-const purgeCSS = {
-  module: require('@fullhuman/postcss-purgecss'),
-  options: {
-    content: [
-      // add extra paths here for components/controllers which include tailwind classes
-      './app/index.html',
-      './app/templates/**/*.hbs',
-    ],
-    defaultExtractor: (content) => content.match(/[A-Za-z0-9-_:/]+/g) || [],
-  },
-};
 
 module.exports = function (defaults) {
   let app = new EmberApp(defaults, {
@@ -29,7 +16,6 @@ module.exports = function (defaults) {
             },
           },
           require('tailwindcss'),
-          ...(isProduction ? [purgeCSS] : []),
         ],
       },
     },
